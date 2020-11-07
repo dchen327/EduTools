14c411ae38a68e46c7b943acc7befc019f11985a
David Chen <davidc3287@gmail.com>
2020-11-07 12:23:28 -0500

    add grammar checking functionality

--- Grammar.py
+++ Grammar.py
@@ -3,13 +3,19 @@ from dotenv import load_dotenv
 import os
 
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-print(os.environ.get('RAPIDAPI_KEY'))
+class Grammar():
 
-client = GrammarBotClient(api_key=rapidapi_key)
+    def __init__(self):
+        dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+        load_dotenv(dotenv_path)
+        rapidapi_key = os.environ.get('RAPIDAPI_KEY')
+        self.client = GrammarBotClient(api_key=rapidapi_key)
 
-text = 'I cant remember how to go their'
+    def check(self, text):
+        res = self.client.check(text)
+        return res.matches
 
-res = client.check(text)
-print(res.matches)
+
+if __name__ == "__main__":
+    grammar = Grammar()
+    print(grammar.check('I cant remember how to go their'))
