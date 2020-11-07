f77f2c83a3bde72ab017e2604ef68a2d1021d8d9
Daniel Li <dan.li2010@hotmail.com>
2020-11-07 11:46:03 -0500

    created rudimentary messaging client

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
 from app import app
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "EduTools welcomes you!"
+
+@app.route('/sms',methods=['GET','POST'])
+def sms_reply():
+    resp = MessagingResponse()
+
+    resp.message("Hello World!")
+
+    return str(resp)
