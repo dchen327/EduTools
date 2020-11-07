36943425a85c73071a81ad551404835445960f83
David Chen <davidc3287@gmail.com>
2020-11-07 17:45:14 -0500

    update code

--- SMSParser.py
+++ SMSParser.py
@@ -1,3 +1,4 @@
+from MathSolver import MathSolver
 from Wikipedia import Wikipedia
 from Translator import Translate
 from Grammar import Grammar
@@ -10,6 +11,7 @@ class SMSParser():
         self.wiki = Wikipedia()
         self.translate = Translate()
         self.grammar = Grammar()
+        self.math_solver = MathSolver()
 
     def commands(self):
         commands = '''List of commands:
@@ -37,23 +39,26 @@ class SMSParser():
                 if action not in wiki_actions:
                     return 'Action not found'
                 text = ' '.join(sections[2:])
+                print(text)
+                print(self.wiki.summary(text))
                 return getattr(self.wiki, action)(text)
             elif feature == 'translate':
                 dest_lang = sections[1]
                 text = ' '.join(sections[2:])
                 return self.translate.translate(text, dest=dest_lang)
             elif feature == 'mathpix':
-                pass
+                equation = ' '.join(sections[1:])
+                return self.math_solver.solve_math2(equation)
             elif feature == 'grammar':
                 text = ' '.join(sections[1:])
-                print(text)
                 return self.grammar.check(text)
-        except:
+        except Exception as e:
+            print(e)
             return 'Something went wrong. Please double-check your command!'
 
 
 if __name__ == "__main__":
     sms_parser = SMSParser()
     command = 'translate en me gusta mucho comer las manzanas'
-    command = 'help'
+    command = 'mathpix 2+3'
     print(sms_parser.parse(command))
