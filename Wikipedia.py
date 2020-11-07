c4417057a6d491c0979d8ff1b19489d14ada64fc
David Chen <davidc3287@gmail.com>
2020-11-07 11:32:15 -0500

    update wikipedia class

--- Wikipedia.py
+++ Wikipedia.py
@@ -1,3 +1,18 @@
 import wikipedia
 
-print(wikipedia.summary('Python (snake)'))
+
+class Wikipedia():
+
+    def search(self, search_term):
+        terms = wikipedia.search(search_term)[:5]
+        message = '<Wikipedia topics>\n'
+        message += '\n'.join(terms)
+        return message
+
+    def summary(self, topic):
+        return wikipedia.summary(topic)
+
+
+if __name__ == "__main__":
+    wiki = Wikipedia()
+    print(wiki.search('code'))
